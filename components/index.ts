3939655e299615f8b449bf12b7403e7dfcb3bfca
Noor Muhammad <connectwithnoor1@gmail.com>
2023-03-23 17:49:27 +0500

    login and register modals

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,21 @@
 import Button from "./button/button";
 import FollowBar from "./followbar/followbar";
 import Header from "./header/header";
+import Input from "./input/Input";
 import Layout from "./layout/layout";
+import LoginModal from "./modal/loginModal";
 import Modal from "./modal/modal";
+import RegisterModal from "./modal/registerModal";
 import SidebarItem from "./sidebar/sidebarItem";
 
-export { Layout, SidebarItem, FollowBar, Header, Modal, Button };
+export {
+  Layout,
+  SidebarItem,
+  FollowBar,
+  Header,
+  Modal,
+  Button,
+  LoginModal,
+  Input,
+  RegisterModal,
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
-import { Layout } from "@/components";
+import { Layout, LoginModal, RegisterModal } from "@/components";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <LoginModal />
+      <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>

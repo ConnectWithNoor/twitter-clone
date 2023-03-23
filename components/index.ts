8b0633e6b94ad8ecbf0628cffb7aa7d01b110fa8
Noor Muhammad <connectwithnoor1@gmail.com>
2023-03-23 17:19:03 +0500

    modal component

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,8 @@
+import Button from "./button/button";
 import FollowBar from "./followbar/followbar";
 import Header from "./header/header";
 import Layout from "./layout/layout";
+import Modal from "./modal/modal";
 import SidebarItem from "./sidebar/sidebarItem";
 
-export { Layout, SidebarItem, FollowBar, Header };
+export { Layout, SidebarItem, FollowBar, Header, Modal, Button };
